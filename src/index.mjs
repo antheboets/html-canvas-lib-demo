9eda434b7ad4c2bcb19374b91a449bd6141743fd
antheboets <anthe.boets@student.ehb.be>
2022-09-10 18:01:49 +0200

    adding logging

--- src/index.mjs
+++ src/index.mjs
@@ -1,16 +1,27 @@
-import  {getCanvas} from 'canvaslib'
+import  {getCanvas} from '../../canvaslib/HtmlCanvasLib.js'
+import {timeStamp} from './helper.mjs'
 
 window.addEventListener("load",async ()=>{
+    timeStamp("dom loaded")
+
     const canvas = getCanvas()
+    timeStamp("canvas created")
+
+    canvas.drawFps = true
 
     canvas.setBackgroundVideo("./background.webm")
+    timeStamp("added background video")
 
     canvas.addImageLayerFromList(["./layer1.png","./layer2.png","./layer3.png"])
+    timeStamp("added first layer")
 
     canvas.addImageLayerFromList(["./layer11.png","./layer12.png"])
+    timeStamp("added second layer")
 
     console.log(canvas.getLayers())
 
     await canvas.startAsync()
+    timeStamp("canvas started")
+
     console.log(canvas,"done")
 })
