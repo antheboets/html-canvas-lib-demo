8f073785adbaefbd3577ddfffcc5b81f02cc2456
antheboets <anthe.boets@student.ehb.be>
2022-10-20 16:17:43 +0200

    making list of webpack module export for multi-file repack

--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,34 @@
 const path = require('path');
 
 const mode = "development"
+const moduleList = []
 
-module.exports = {
+
+//https://stackoverflow.com/questions/35903246/how-to-create-multiple-output-paths-in-webpack-config
+var config = {
+  // TODO: Add common Configuration
   mode: mode,
+  module: {},
+};
+
+moduleList.push(Object.assign({}, config, {
+  name: "index",
   entry: './src/index.mjs',
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
   },
-};
-
-module.exports = {
-  mode: mode,
+}))
+moduleList.push(Object.assign({}, config,{
+  name: "randomTimer",
   entry: './src/randomTimer.mjs',
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist/randomTimer'),
   },
-};
+}))
+
+// Return Array of Configurations
+module.exports = [
+  moduleList
+];
