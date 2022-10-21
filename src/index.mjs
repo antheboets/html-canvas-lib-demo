7dce637763b5cb17e9c9a1e48004bc27c44f6131
antheboets <anthe.boets@student.ehb.be>
2022-10-22 00:14:39 +0200

    index page

--- src/index.mjs
+++ src/index.mjs
@@ -1,27 +1,60 @@
 import  {getCanvas} from 'canvaslib'
-import {timeStamp} from './helper.mjs'
+import {timeStamp, printWelcome} from './helper.mjs'
+
+printWelcome()
 
 window.addEventListener("load",async ()=>{
     timeStamp("dom loaded")
+
+    let active = false
 
     const canvas = getCanvas()
     timeStamp("canvas created")
 
     canvas.drawFps = true
 
-    canvas.setBackgroundVideo("./background.webm")
+    canvas.setBackgroundVideo("./content/WinningAMeal.webm")
     timeStamp("added background video")
 
-    canvas.addImageLayerFromList(["./layer1.png","./layer2.png","./layer3.png"])
+    //canvas.addLayerFromList([{path:"./layer1.png",time:()=>{console.log("layer1");return 3 * 1000;}},{path:"./layer2.png",time:()=>{randomNumber(0,5)*1000}},{path:"./layer3.png",time:1 * 1000}])
+    //timeStamp("added first layer")
+
+    canvas.createLayer([
+        {contentType:"image",path:"./content/backgroundLayer1.png",time:3 * 1000},
+        {contentType:"image",path:"./content/backgroundLayer2.png",time:3 * 1000},
+        {contentType:"image",path:"./content/backgroundLayer3.png",time:3 * 1000}
+    ])
+    canvas.getLayer(0).setMode = 'timer'
     timeStamp("added first layer")
 
-    canvas.addImageLayerFromList(["./layer11.png","./layer12.png"])
+    //canvas.addLayerFromList([{path:"./layer11.png",time:1* 1000},{path:"./layer12.png",time:1 * 1000}])
+    //canvas.createLayer([{path:"./random1.png",time:()=>{timeStamp("random1");return 10 * 1000}},{path:"./random2.png",time:()=>{timeStamp("random2");return 3 * 1000}},{path:"./layer1.png",time:()=>{timeStamp("layer1");return 5 * 1000}}])
+    canvas.createLayer([
+        "./content/helloWorld1.png",
+        "./content/helloWorld2.png"
+    ])
     timeStamp("added second layer")
 
-    console.log(canvas.getLayers())
+    console.log(canvas.getLayers(),"layers")
+    console.log(canvas.getLayer(0),"layer")
+
+    //canvas.getLayer(0).setMode = 'timer'
+    //console.log(canvas.getLayer(0))
 
     await canvas.startAsync()
     timeStamp("canvas started")
+    active = true
 
     console.log(canvas,"done")
+    window.addEventListener("mousedown",async (e)=>{
+        e.preventDefault
+        if(active){
+            if(e.wich === 3){
+                canvas.getLayer(1).previous()
+            }
+            else{
+                canvas.getLayer(1).next()
+            }
+        }
+    })
 })
