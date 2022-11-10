8de56b68c4a65c71b80261ce0c7f4195570e106d
antheboets <anthe.boets@student.ehb.be>
2022-11-10 16:18:20 +0100

    beter weboacp code

--- webpack.config.js
+++ webpack.config.js
@@ -1,54 +1,36 @@
-const path = require('path');
-const fs = require('fs');
+const path = require('path')
+const fs = require('fs')
 
 const mode = "development"
 const moduleList = []
 
-
-
 //https://stackoverflow.com/questions/35903246/how-to-create-multiple-output-paths-in-webpack-config
-var config = {
+const config = {
   // TODO: Add common Configuration
   mode: mode,
   module: {},
-};
-
-moduleList.push(Object.assign({}, config, {
-  name: "index",
-  entry: './src/index.mjs',
-  output: {
-    filename: 'main.js',
-    path: path.resolve(__dirname, 'dist'),
-  },
-}))
-moduleList.push(Object.assign({}, config,{
-  name: "randomTimer",
-  entry: './src/randomTimer.mjs',
-  output: {
-    filename: 'main.js',
-    path: path.resolve(__dirname, 'dist/randomTimer'),
-  },
-}))
-if(fs.existsSync('src/debugPlayground.mjs')){
-  moduleList.push(Object.assign({}, config,{
-    name: "debugPlayground",
-    entry: './src/debugPlayground.mjs',
-    output: {
-      filename: 'main.js',
-      path: path.resolve(__dirname, 'dist/debugPlayground'),
-    },
-  }))
 }
-moduleList.push(Object.assign({}, config,{
-  name: "clock",
-  entry: './src/clock.mjs',
-  output: {
-    filename: 'main.js',
-    path: path.resolve(__dirname, 'dist/clock'),
-  },
-}))
+
+function addModule(name,entry,dir=''){
+  if(fs.existsSync(entry)){
+    console.log(entry)
+    moduleList.push(Object.assign({}, config), {
+      name: name,
+      entry: entry,
+      output:{
+        filename: 'main.js',
+        path: path.resolve(__dirname, `./dist${dir}`)
+      }
+    })
+  }
+}
+
+addModule('index','./src/index.mjs')
+addModule('randomTimer','./src/randomTimer.mjs','/randomTimer')
+addModule('debugPlayground','./src/debugPlayground.mjs','/debugPlayground')
+addModule('clock','./src/clock.mjs','/clock')
 
 // Return Array of Configurations
 module.exports = [
   moduleList
-];
+]
