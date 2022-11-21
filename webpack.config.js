92dc2623cca8246c96f1ed4eab78be189037313b
antheboets <anthe.boets@student.ehb.be>
2022-11-22 00:23:21 +0100

    removing console.log

--- webpack.config.js
+++ webpack.config.js
@@ -4,17 +4,9 @@ const fs = require('fs')
 const mode = "development"
 const moduleList = []
 
-//https://stackoverflow.com/questions/35903246/how-to-create-multiple-output-paths-in-webpack-config
-const config = {
-  // TODO: Add common Configuration
-  mode: mode,
-  module: {},
-}
-
 function addModule(name,entry,dir=''){
   if(fs.existsSync(entry)){
     console.log(entry)
-    moduleList.push(Object.assign({}, config), {
       name: name,
       entry: entry,
       output:{
