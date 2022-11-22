aa9f824656439d44d97785a2d60424d7b31d3113
antheboets <anthe.boets@student.ehb.be>
2022-11-22 01:44:16 +0100

    adding args for optimization mode and adding documentation

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,33 @@
 const path = require('path')
 const fs = require('fs')
 
-const mode = "development"
+//getting the args
+const args = process.argv.slice(2)
+//of all the modules that will be exported
 const moduleList = []
-
+//mode of code optimization development/production/none
+let mode = "production"
+//getting mode out of args
+for(let i = 0; i < args.length; i++){
+    if(args[i] === "--env"){
+        switch(args[i+1]){
+            case "dev":
+                mode = "development"
+            break
+            case "prod":
+                mode = "production"
+            break
+            case "none":
+                mode = "none"
+            break
+            default:
+                mode = "production"
+            break
+        }
+        i = Infinity
+    }
+}
+//function that creates a module object and add it to the list
 function addModule(name,entry,dir=''){
     if(fs.existsSync(entry)){
             moduleList.push({
@@ -17,11 +41,11 @@ function addModule(name,entry,dir=''){
         })
     }
 }
-
+//adding modules
 addModule('index','./src/index.mjs')
 addModule('randomTimer','./src/randomTimer.mjs','/randomTimer')
 addModule('debugPlayground','./src/debugPlayground.mjs','/debugPlayground')
 addModule('clock','./src/clock.mjs','/clock')
 addModule('video','./src/video.mjs','/video')
-
+//exporting modules
 module.exports = moduleList
