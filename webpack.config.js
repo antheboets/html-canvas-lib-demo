6ca10fcad7059b6f2fc91794b1be260dfd52842b
antheboets <anthe.boets@student.ehb.be>
2022-11-24 23:07:15 +0100

    rewriting the way webpack config deals with args

--- webpack.config.js
+++ webpack.config.js
@@ -1,32 +1,42 @@
 const path = require('path')
 const fs = require('fs')
+
 //getting the args
-const args = process.argv.slice(2)
-//of all the modules that will be exported
-const moduleList = []
-//mode of code optimization development/production/none
-let mode = "production"
-//getting mode out of args
-for(let i = 0; i < args.length; i++){
-    if(args[i] === "--env"){
-        switch(args[i+1]){
-            case "dev":
-                mode = "development"
-            break
-            case "prod":
-                mode = "production"
-            break
-            case "none":
-                mode = "none"
-            break
-            default:
-                mode = "production"
-            break
-        }
-        //i have no idea if this works and if this is a good thing but it seems to work
-        i = Infinity
+const argv = process.argv.slice(2)
+const args = {}
+
+//putting args in a key value pair
+let foundEnv  = false
+for(let i = 0; i < argv.length; i++){
+    if(foundEnv){
+        let keyValuePair = argv[i].split('=')
+        args[keyValuePair[0]] = keyValuePair[1]
+    }
+    if(argv[i] === "--env" && !foundEnv){
+        foundEnv = true
     }
 }
+
+//mode of code optimization development/production/none
+let mode = ""
+switch(args.optimazation){
+    case "dev":
+        mode = "development"
+    break
+    case "prod":
+        mode = "production"
+    break
+    case "none":
+        mode = "none"
+    break
+    default:
+        mode = "production"
+    break
+}
+
+//of all the modules that will be exported
+const moduleList = []
+
 //function that creates a module object and add it to the list
 function addModule(name,entry,dir=''){
     if(fs.existsSync(entry)){
@@ -41,6 +51,8 @@ function addModule(name,entry,dir=''){
         })
     }
 }
+
+//this should be a function that reads the dir maybe?
 //adding modules
 addModule('index','./src/index.mjs')
 addModule('randomTimer','./src/randomTimer.mjs','/randomTimer')
@@ -48,5 +60,6 @@ addModule('debugPlayground','./src/debugPlayground.mjs','/debugPlayground')
 addModule('clock','./src/clock.mjs','/clock')
 addModule('video','./src/video.mjs','/video')
 addModule('video2','./src/video2.mjs','/video2')
+
 //exporting modules
 module.exports = moduleList
