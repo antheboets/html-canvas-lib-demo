dc4de07970b75100a8a934601afe4bce60488179
antheboets <anthe.boets@student.ehb.be>
2022-11-30 22:43:53 +0100

    chaning index page so it has buttons to all the other pages

--- src/index.mjs
+++ src/index.mjs
@@ -17,9 +17,10 @@ window.addEventListener("load",async ()=>{
     let video = BackgroundVideo()
     video.path = "./content/WinningAMeal.webm"
 
+    console.log(video)
     canvas.createLayer(video)
     timeStamp("added background video layer")
-
+    /*
     canvas.createLayer([
         {contentType:"image",path:"./content/backgroundLayer1.png",time:3 * 1000},
         {contentType:"image",path:"./content/backgroundLayer2.png",time:3 * 1000},
@@ -34,7 +35,13 @@ window.addEventListener("load",async ()=>{
     ])
     timeStamp("added third layer")
 
-    canvas.createLayer([{contentType:"video",path:"./content/ANontypicalDayAtWork.webm",y:"50%",height:"50%"}])
+    canvas.createLayer([{contentType:"video",path:"./content/ANontypicalDayAtWork.webm",y:"25%",x:"25%",height:"25%",width:"25%", click:true,clickAction:()=>{window.location.replace("video2/index.html")}}])
+    */
+
+    canvas.createLayer([{contentType:"image",path:"./content/linkButton.png",x:40,y:50,click:true,clickAction:()=>{window.location.replace("./clock/index.html")}}])
+    canvas.createLayer([{contentType:"image",path:"./content/linkButton.png",x:40,y:300,click:true,clickAction:()=>{window.location.replace("./randomTimer/index.html")}}])
+    canvas.createLayer([{contentType:"image",path:"./content/linkButton.png",x:40,y:550,click:true,clickAction:()=>{window.location.replace("./video/index.html")}}])
+    canvas.createLayer([{contentType:"image",path:"./content/linkButton.png",x:40,y:800,click:true,clickAction:()=>{window.location.replace("./video2/index.html")}}])
 
     await canvas.startAsync()
     timeStamp("canvas started")
