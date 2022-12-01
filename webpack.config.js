c83ab706683aa50c64ce67fa69ba2b70841877c2
antheboets <anthe.boets@student.ehb.be>
2022-12-01 20:23:53 +0100

    adding list of content to pre bundeling

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path')
 const fs = require('fs')
+const fsPromises = require('fs').promises
 
 //getting the args
 const argv = process.argv.slice(2)
@@ -51,6 +52,42 @@ function addModule(name,entry,dir=''){
         })
     }
 }
+const contentList = []
+
+async function writeJsonFile (filename,data){
+    await fsPromises.writeFile(`${filename}.json`,JSON.stringify(data,null,3),(error)=>{})
+}
+
+async function recursiveThroughDir(pathOfDir){
+    let listOfFilesInDir = await fsPromises.readdir(pathOfDir)
+    //console.log(listOfFilesInDir)
+    //console.log("async?")
+    await listOfFilesInDir.forEach(async (file)=>{
+        if(isDir(path.resolve(pathOfDir,file))){
+            //console.log("async?2")
+            let test = await recursiveThroughDir(path.resolve(pathOfDir,file))
+        }
+        else{
+            //console.log(`${pathOfDir.replace("/dist","")}${file}`)
+            contentList.push(`${pathOfDir.replace("/dist","")}${file}`)
+        }
+    })
+}
+
+function isDir(pathOfDir){
+    return fs.existsSync(pathOfDir) && fs.lstatSync(pathOfDir).isDirectory() 
+}
+
+async function makeListForContentPage(){
+    await recursiveThroughDir("./dist/content/")
+    console.log(contentList)
+    await writeJsonFile("./dist/content",contentList)
+}
+
+function preBundeling(){
+    makeListForContentPage()
+}
+preBundeling()
 
 //this should be a function that reads the dir maybe?
 //adding modules
@@ -61,6 +98,8 @@ addModule('clock','./src/clock.mjs','/clock')
 addModule('video','./src/video.mjs','/video')
 addModule('video2','./src/video2.mjs','/video2')
 addModule('content','./src/content.mjs','/content')
+
+//postBundeling if needed()
 
 console.log(`bundeling with ${mode} optimazation mode`)
 
